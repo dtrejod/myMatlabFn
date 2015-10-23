79084a94a4d40f3d1971fc8f60cdbe520a671c64
Devin Trejo <dtrejod@gmail.com>
2015-10-23 13:17:10 -0400

    Use export Figures to export scaled figures

--- printFigures.m
+++ printFigures.m
@@ -1,6 +1,8 @@
 % Program Print figures to current folder
 % By: Devin Trejo
 
+% Requires 'export_fig' in your path: https://github.com/altmany/export_fig
+
 %%
 function printFigures()
 
@@ -8,6 +10,10 @@ function printFigures()
     
     h = get(0,'children');
     for i=1:length(h)
-      print(sprintf('-f%.1d',i),'-dpng', [pathName [fileName(1:length(fileName)-4) '_' num2str(i) '.png']])
+        set(h(i).CurrentAxes, 'Color', 'none');
+        set(h(i), 'units', 'inches', 'Position', [0, 0, 7, 2]);
+        saveFileName = [pathName [fileName(1:length(fileName)-4) ...
+            '_' num2str(i) '.png']];
+        export_fig(h(i), saveFileName, '-transparent', '-m2.5');
     end
 end
