ad90efb45896fcad3ad5358989cebc56799016d7
Devin Trejo <dtrejod@gmail.com>
2015-11-11 13:21:07 -0500

    PrintFigures for IEEE and Normal

--- printFiguresIEEE.m
+++ printFiguresIEEE.m
@@ -1,16 +1,20 @@
 % Program Print figures to current folder
 % By: Devin Trejo
 
+% Print Figures for IEEE formatted Column Paper
+% Requires 'export_fig' in your path: https://github.com/altmany/export_fig
+
 %%
-function printFigures()
+function printFiguresIEEE()
 
     [fileName, pathName] = uiputfile('*.png');
     
     h = get(0,'children');
     for i=1:length(h)
         set(h(i).CurrentAxes, 'Color', 'none');
+        set(h(i), 'units', 'inches', 'Position', [0, 0, 7, 2]);
         saveFileName = [pathName [fileName(1:length(fileName)-4) ...
             '_' num2str(i) '.png']];
-        export_fig(h(i), saveFileName, '-transparent');
+        export_fig(h(i), saveFileName, '-transparent', '-m2.5');
     end
 end
